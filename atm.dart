f11503ce5cc35bb65f99f36c046d10e15637c702
nouman <m.nouman.hayat>
2022-04-08 11:06:21 +0500

    fdf

--- atm.dart
+++ atm.dart
@@ -119,3 +119,150 @@ import 'dart:io';
 //   print('factorial is $fact');
 // }
 
+// void main(List<String> args) {
+//   print('enter number');
+//   String? numstr = stdin.readLineSync();
+//   int num = int.parse(numstr!);
+//   evenodd(n: num);
+// }
+
+// void evenodd({required int n}) {
+//   int c = 0;
+//   if (n > 1) {
+//     for (int i = 2; i <= n ~/ 2; i++) {
+//       if (n % i == 0) {
+//         c = 1;
+//       }
+//     }
+//     if (n % 2 == 0) {
+//       print('number $n is even ');
+//     } else if (n % 2 != 0) {
+//       print('number $n is odd ');
+//     }
+//     if (c == 0) {
+//       print('$n is prime');
+//     } else if (c != 0) {
+//       print('$n is not prime');
+//     }
+//   } else {
+//     print('Number should be greater then 1');
+//   }
+// }
+
+// void main(List<String> args) {
+//   print('enter number 1');
+//   String? num1str = stdin.readLineSync();
+//   int num1 = int.parse(num1str!);
+//   print('enter number 2');
+//   String? num2str = stdin.readLineSync();
+//   int num2 = int.parse(num2str!);
+//   var a, b = swapp(a: num1, b: num2);
+// }
+
+// void swapp({required int a, required int b}) {
+//   int c;
+//   c = a + b;
+//   b = c - b;
+//   a= c - a;
+
+// print(a);
+// print(b);
+// }
+
+// void main(List<String> args) {
+//   print('enter number');
+//   String? numstr = stdin.readLineSync();
+//   int num = int.parse(numstr!);
+//   grade(n: num);
+// }
+
+// void grade({required int n}) {
+//   if (n >= 80) {
+//     print('grade A');
+//   } else if (n >= 60 && n <= 79) {
+//     print('Grade b');
+//   } else if (n >= 40 && n <= 59) {
+//     print('Grade c');
+//   } else if (n < 40) {
+//     print('Grade d');
+//   }
+// }
+
+// void main(List<String> args) {
+//   print('enter pair of integar');
+//   String? num1str = stdin.readLineSync();
+//   int num1 = int.parse(num1str!);
+//   String? num2str = stdin.readLineSync();
+//   int num2 = int.parse(num2str!);
+//   bool check= mul(num1, num2);
+//   if(check){
+//      print('$num2 is the multiple of $num1');
+//   }
+//   else{
+// print('$num2 is not multiple of $num1');
+//   }
+// }
+
+// bool mul( int a,  int b) {
+//  return b % a == 0;
+// }
+
+// void main(List<String> args) {
+//   print('enter base and height');
+//   print(area(input(), input()));
+// }
+
+// int input() {
+
+//   String? bstr = stdin.readLineSync();
+//   int b = int.parse(bstr!);
+//   return b;
+// }
+
+// double area(int a, int b) {
+//   double ar = 0.5 * a * b;
+//   return ar;
+// }
+
+// void main(List<String> args) {
+//   print('enter two number');
+//   int sq = input();
+//    int cub = input();
+//   print('square of ${sq} is ${square(sq)}');
+//   print('cube of ${cub} is ${cube(cub)}');
+// }
+
+// int input() {
+//   String? bstr = stdin.readLineSync();
+//   int b = int.parse(bstr!);
+//   return b;
+// }
+
+// int square(int a) {
+//   return a * a;
+// }
+
+// int cube(int b) {
+//   return b * b * b;
+// }
+
+void main(List<String> args) {
+  print('enter two number');
+  int num1 = input();
+  int num2 = input();
+  int res = gcd(num1, num2);
+}
+
+int input() {
+  String? bstr = stdin.readLineSync();
+  int b = int.parse(bstr!);
+  return b;
+}
+
+int gcd(int num1, int num2) {
+  int res = 0;
+  for (int i = 0; i <= num1; i++) {
+    if()
+  }
+  return res;
+}
